70b64169403d49d4840e534ac5bfcb7884cfd41c
584702675@qq.com <123456789@qq.com>
2018-05-08 17:27:52 +0800

    1

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -38,7 +38,7 @@ export function validatAlphabets(str) {
 }
 /* 手机号码校验*/
 export function phoneCase(str) {
-  const reg = /^1[3|4|5|8][0-9]\d{4,8}$/;
+  const reg = /^1[2|3|4|5|6|7|8|9][0-9]\d{4,8}$/;
   return reg.test(str);
 }
 
